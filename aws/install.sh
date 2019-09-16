a4606ca07dc12c3569bcbf33172614fda78730b1
Jason A. Boyll <jason.boyll@gmail.com>
2019-09-15 22:52:15 -0400

    Add aws installer

---
+++ aws/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Go Installation
+
+
+source ../lib/utils.sh
+source ../environment.sh
+
+
+banner "AWS-CLI"
+
+
+if [[ ! -e "${HOME}/.local/bin/aws" ]]; then
+  su -c "pip3 install awscli --upgrade --user" "$_USER"
+else
+  echo "AWS-CLI appears to already be installed. Skipping..."
+fi
