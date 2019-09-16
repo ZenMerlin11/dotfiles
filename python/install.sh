564fe04c863b9042ae9e75f0f3512093e0c3aa60
Jason A. Boyll <jason.boyll@gmail.com>
2019-09-15 21:47:05 -0400

    Install python packages

---
+++ python/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install python37, pip, venv
+
+
+source ../lib/utils.sh
+source ../environment.sh
+
+
+install python3-pip
+install python3-pip
+install build-essential
+install libssl-dev
+install libffi-dev
+install python3-dev
+
+if [[ ! -e "${_HOME}/.local/bin/virtualenv" ]]; then
+  echo "Installing virtualenv..."
+  su -c "pip3 install virtualenv" "$_USER"
+else
+  echo "virtualenv already installed. Skipping..."
+fi
