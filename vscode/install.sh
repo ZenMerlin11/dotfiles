00b8261dfbd7e9043e8eab34f90346b22022d592
Jason Boyll <jason.boyll@gmail.com>
2019-09-16 19:57:34 -0400

    Add vscode installer

---
+++ vscode/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install VS Code
+
+
+source ../lib/utils.sh
+source ../environment.sh
+
+
+banner "VSCode"
+
+
+if not_installed code; then
+  snap install code --classic
+else
+  echo "VS Code already installed. Skipping..."
+fi
