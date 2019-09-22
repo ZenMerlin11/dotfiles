9f632df1b3247b58a32d67af3565226a16e5a96b
Jason Boyll <jason.boyll@gmail.com>
2019-09-22 12:03:46 -0400

    Remap caps_lock key as i3 mod key

--- i3/install.sh
+++ i3/install.sh
@@ -11,4 +11,6 @@ banner "i3"
 
 install i3
 install feh
-link_files "$(pwd)" "${_HOME}/.config/i3"
+mkdir -p "${_HOME}/.config/i3"
+link_file "$(pwd)/config.symlink" "${_HOME}/.config/i3/config"
+link_file "$(pwd)/.Xmodmap.symlink" "${_HOME}/.Xmodmap"
