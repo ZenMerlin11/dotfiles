3651665544c1e514065c2a39db6210812f4a289a
Jason Boyll <jason.boyll@gmail.com>
2020-05-16 21:35:24 -0400

    Change terraform setup to use tgenv/tfenv

--- terraform/install.sh
+++ terraform/install.sh
@@ -5,61 +5,12 @@
 source ../lib/utils.sh
 source ../environment.sh
 
-TERRAFORM_VERSION="0.12.7"
-TERRAGRUNT_VERSION="0.19.21"
-
-banner "Terraform"
-
-
-manual_install() {
-  program="$1"
-  version="$2"
-  install_path="$3"
-
-  echo "Attempting to install ${program} v${version}..."
-
-  if is_installed "$program"; then
-    echo "Existing binary detected at $(command -v "$program"). Skipping..."
-  else
-    if [[ "$program" == "terraform" ]]; then
-      install_terraform "$version" "$install_path"
-    else
-      install_terragrunt "$version" "$install_path"
-    fi
-  fi
-}
-
-
-install_terraform() {
-  version="$1"
-  install_path="$2"
-
-  echo "Downloading Terraform v${version}..."
-  wget "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"\
-    -qO terraform.zip
-
-  echo "Installing Terraform v${version} binary to ${install_path}/terraform..."
-  unzip terraform.zip && \
-  mv terraform "${install_path}/terraform" && \
-    chmod 755 "${install_path}/terraform" && \
-    rm terraform.zip && \
-    echo "Terraform installed!"
-}
-
-
-install_terragrunt() {
-  version="$1"
-  install_path="$2"
-
-  echo "Downloading Terragrunt v${version}..."
-  wget "https://github.com/gruntwork-io/terragrunt/releases/download/v${version}/terragrunt_linux_amd64" \
-    -qO terragrunt
-
-  echo "Installing Terragrunt v${version} binary to ${install_path}/terragrunt..."
-  mv terragrunt "${install_path}/terragrunt" && \
-    chmod 755 "${install_path}/terragrunt" && \
-    echo "Terragrunt installed!"
-}
-
-manual_install terraform "$TERRAFORM_VERSION" '/usr/local/bin'
-manual_install terragrunt "$TERRAGRUNT_VERSION" '/usr/local/bin'
+# Install tfenv and tgenv
+tfenv_dir="${_HOME}/.tfenv"
+tgenv_dir="${_HOME}/.tgenv"
+if [[ ! -d "$tfenv_dir" ]]; then
+  git clone https://github.com/tfutils/tfenv.git "$tfenv_dir"
+fi
+if [[ ! -d "$tgenv_dir" ]]; then
+  git clone https://github.com/cunymatthieu/tgenv.git "$tgenv_dir"
+fi
---
+++ terraform/tfrc.zsh
@@ -0,0 +1,3 @@
+# Add .tfenv and .tgenv to PATH
+export PATH="${PATH}:${HOME}/.tfenv/bin:${HOME}/.tgenv/bin"
+
