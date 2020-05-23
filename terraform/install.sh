f0203b01ecafffd03b3c25acf50a2e5a6305f644
Jason Boyll <jason.boyll@gmail.com>
2020-05-23 12:21:35 -0400

    Log when skipping install

--- terraform/install.sh
+++ terraform/install.sh
@@ -12,7 +12,11 @@ tfenv_dir="${_HOME}/.tfenv"
 tgenv_dir="${_HOME}/.tgenv"
 if [[ ! -d "$tfenv_dir" ]]; then
   su -c "git clone https://github.com/tfutils/tfenv.git ${tfenv_dir}" "$_USER"
+else
+  echo "tfenv already installed. Skipping..."
 fi
 if [[ ! -d "$tgenv_dir" ]]; then
   su -c "git clone https://github.com/cunymatthieu/tgenv.git ${tgenv_dir}" "$_USER"
+else
+  echo "tgenv already installed. Skipping..."
 fi
