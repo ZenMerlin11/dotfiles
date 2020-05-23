110d20f25f21cb1ab2c164d6b046d36744c618f4
Jason Boyll <jason.boyll@gmail.com>
2020-05-23 12:16:21 -0400

    Clone repos as _USER to avoid permissions issues

--- terraform/install.sh
+++ terraform/install.sh
@@ -5,12 +5,14 @@
 source ../lib/utils.sh
 source ../environment.sh
 
+banner "Terraform"
+
 # Install tfenv and tgenv
 tfenv_dir="${_HOME}/.tfenv"
 tgenv_dir="${_HOME}/.tgenv"
 if [[ ! -d "$tfenv_dir" ]]; then
-  git clone https://github.com/tfutils/tfenv.git "$tfenv_dir"
+  su -c "git clone https://github.com/tfutils/tfenv.git ${tfenv_dir}" "$_USER"
 fi
 if [[ ! -d "$tgenv_dir" ]]; then
-  git clone https://github.com/cunymatthieu/tgenv.git "$tgenv_dir"
+  su -c "git clone https://github.com/cunymatthieu/tgenv.git ${tgenv_dir}" "$_USER"
 fi
